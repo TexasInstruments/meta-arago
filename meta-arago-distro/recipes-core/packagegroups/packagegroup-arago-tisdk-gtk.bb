TexasInstruments/meta-arago

commit 731fd37628fdfb701a4f6ebfe4840eea5de7a948

packagegroups: Do not set package arch when unneeded
These package groups do not change based on the machine,
do not set PACKAGE_ARCH as that forces these to be built
for every machine anyway.

Signed-off-by: Andrew Davis <afd@ti.com>
Signed-off-by: Ryan Eatmon <reatmon@ti.com>

meta-arago-distro/recipes-core/packagegroups/packagegroup-arago-tisdk-gtk.bb
@@ -2,8 +2,6 @@ DESCRIPTION = "Task to add Gtk embedded related packages"
 LICENSE = "MIT"
 PR = "r2"
 
-PACKAGE_ARCH = "${MACHINE_ARCH}"
-
 inherit packagegroup
 
 RDEPENDS:${PN} = "\

meta-arago-distro/recipes-core/packagegroups/packagegroup-arago-base-tisdk-server-extra.bb
@@ -3,8 +3,6 @@ LICENSE = "MIT"
 LIC_FILES_CHKSUM = "file://${COREBASE}/meta/COPYING.MIT;md5=3da9cfbcb788c80a0384361b4de20420"
 PR = "r17"
 
-PACKAGE_ARCH = "${MACHINE_ARCH}"
-
 inherit packagegroup
 
 ARAGO_LIBNL = "\

meta-arago-distro/recipes-core/packagegroups/packagegroup-arago-tisdk-crypto-sdk-target.bb
@@ -2,8 +2,6 @@ DESCRIPTION = "Task to install crypto dev packages in SDK"
 LICENSE = "MIT"
 PR = "r8"
 
-PACKAGE_ARCH = "${MACHINE_ARCH}"
-
 inherit packagegroup
 
 CRYPTO_API = "cryptodev-module-dev"

meta-arago-distro/recipes-core/packagegroups/packagegroup-arago-tisdk-crypto.bb
@@ -2,8 +2,6 @@ DESCRIPTION = "Task to install crypto packages into target FS"
 LICENSE = "MIT"
 PR = "r13"
 
-PACKAGE_ARCH = "${MACHINE_ARCH}"
-
 inherit packagegroup
 
 CRYPTO_SUPPORT = "\

meta-arago-distro/recipes-core/packagegroups/packagegroup-arago-tisdk-gtk-sdk-target.bb
@@ -2,8 +2,6 @@ DESCRIPTION = "Task to install Gtk dev packages in SDK"
 LICENSE = "MIT"
 PR = "r2"
 
-PACKAGE_ARCH = "${MACHINE_ARCH}"
-
 inherit packagegroup
 
 RDEPENDS:${PN} = "\

commit e2a0702d4ca5d2ac51b0b7eaaf59d4b56234b299

packagegroup-arago-tisdk-gtk: set PACKAGE_ARCH
This packagegroup pulls a package that is automatically renamed with
DEBIAN_NAMES. This is not allowed due to a sanity check that was added
to prevent issues with packagegroups being aggressively cached and
DEBIAN_NAMES potentially breaking this cache. See [YOCTO #7298] or
5bf3e447d2f in openembedded-core [1].

[1] https://git.openembedded.org/openembedded-core/commit/?id=5bf3e447d2f5064495d83a8fad30229bcf1ecc9b

Fixes: 731fd376 (packagegroups: Do not set package arch when unneeded, 2025-03-03)
Signed-off-by: Randolph Sapp <rs@ti.com>
Signed-off-by: Ryan Eatmon <reatmon@ti.com>

meta-arago-distro/recipes-core/packagegroups/packagegroup-arago-tisdk-gtk.bb
@@ -2,6 +2,10 @@ DESCRIPTION = "Task to add Gtk embedded related packages"
 LICENSE = "MIT"
 PR = "r2"
 
+# Required for packages that are renamed using DEBIAN_NAMES
+# [YOCTO #7298]
+PACKAGE_ARCH = "${TUNE_PKGARCH}"
+
 inherit packagegroup
 
 RDEPENDS:${PN} = "\