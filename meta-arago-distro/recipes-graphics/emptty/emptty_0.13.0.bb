TexasInstruments/meta-arago

commit 67d7748a001c3f3f4aea1073422cc85a9adf699d

emptty: address feedback around append usage
Shy away from using append when not strictly necessary. Move the pam
specific runtime dependency under the relevant PACKAGECONFIG.

Signed-off-by: Randolph Sapp <rs@ti.com>
Signed-off-by: Ryan Eatmon <reatmon@ti.com>

meta-arago-distro/recipes-graphics/emptty/emptty_0.13.0.bb
@@ -16,23 +16,25 @@ SRCREV_pam = "50ded1b0e7864b9bf75005eb945a8ec826bcf69d"
 
 SRCREV_FORMAT .= "_pam"
 
-PACKAGES:append = " ${PN}-conf"
+PACKAGES += "${PN}-conf"
 
 PACKAGECONFIG ?= "${@bb.utils.filter('DISTRO_FEATURES', 'pam x11', d)}"
-PACKAGECONFIG[pam] = ",,libpam"
+PACKAGECONFIG[pam] = ",,libpam,pam-plugin-succeed-if"
 PACKAGECONFIG[x11] = ",,virtual/libx11"
+
+DEPENDS += "${@bb.utils.contains('PACKAGECONFIG', 'pam', '', 'libxcrypt', d)}"
 
 GO_TAGS = ""
 GO_TAGS:append = "${@bb.utils.contains('PACKAGECONFIG', 'pam', '', ',nopam', d)}"
 GO_TAGS:append = "${@bb.utils.contains('PACKAGECONFIG', 'x11', '', ',noxlib', d)}"
 
-GOBUILDFLAGS:append = " -tags=${GO_TAGS}"
+GOBUILDFLAGS += "-tags=${GO_TAGS}"
 
 export GO111MODULE="off"
 
 inherit go update-rc.d systemd
 
-DEPENDS:append = " gzip"
+DEPENDS += "gzip-native"
 
 do_install () {
     # general collateral
@@ -64,11 +66,11 @@ FILES:${PN} = "\
     ${sysconfdir}/pam.d/emptty \
 "
 
-FILES:${PN}-conf:append = " ${sysconfdir}/emptty/conf"
-CONFFILES:${PN}-conf:append = " ${sysconfdir}/emptty/conf"
-RPROVIDES:${PN}-conf:append = " virtual-emptty-conf"
+FILES:${PN}-conf += "${sysconfdir}/emptty/conf"
+CONFFILES:${PN}-conf += "${sysconfdir}/emptty/conf"
+RPROVIDES:${PN}-conf += "virtual-emptty-conf"
 
-RDEPENDS:${PN}:append = " virtual-emptty-conf pam-plugin-succeed-if"
+RDEPENDS:${PN} += "virtual-emptty-conf"
 
 SYSTEMD_SERVICE:${PN} = "emptty.service"
 

commit e7b1cadc500cf6ffac470b652e2da9e5d4e6ab42

emptty: use of append in build flags
The use of append with GOBUILDFLAGS is mandatory. There is no reasonable
extra buildflags variable to use in this case, and using += will break
argument ordering.

Fixes: 67d7748a (emptty: address feedback around append usage, 2025-03-19)
Signed-off-by: Randolph Sapp <rs@ti.com>
Signed-off-by: Ryan Eatmon <reatmon@ti.com>

meta-arago-distro/recipes-graphics/emptty/emptty_0.13.0.bb
@@ -28,7 +28,7 @@ GO_TAGS = ""
 GO_TAGS:append = "${@bb.utils.contains('PACKAGECONFIG', 'pam', '', ',nopam', d)}"
 GO_TAGS:append = "${@bb.utils.contains('PACKAGECONFIG', 'x11', '', ',noxlib', d)}"
 
-GOBUILDFLAGS += "-tags=${GO_TAGS}"
+GOBUILDFLAGS:append = " -tags=${GO_TAGS}"
 
 export GO111MODULE="off"
 